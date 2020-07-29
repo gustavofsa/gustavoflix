0d1119036cc263c41199f576a6908331574cc03d
Gustavo Farias <gugafarias2009@gmail.com>
2020-07-28 23:47:16 -0300

    Add: Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,42 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
 
-import Header from './components/Header'
+import dadosIniciais from "./data/dados_iniciais.json";
+
+import Header from "./components/Header";
+import BannerMain from "./components/BannerMain";
+import Carousel from "./components/Carousel";
+import Footer from "./components/Footer";
+
+const AppWrapper = styled.div`
+  background-color: var(--grayDark);
+`;
 
 function App() {
   return (
-    <div>
+    <AppWrapper>
       <Header />
-    </div>
+
+      <BannerMain
+        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+        url={dadosIniciais.categorias[0].videos[0].url}
+        videoDescription={"O que é Front-end? Trabalhando na área"}
+      />
+
+      <Carousel ignoreFirstVideo category={dadosIniciais.categorias[0]} />
+
+      <Carousel category={dadosIniciais.categorias[1]} />
+
+      <Carousel category={dadosIniciais.categorias[2]} />
+
+      <Carousel category={dadosIniciais.categorias[3]} />
+
+      <Carousel category={dadosIniciais.categorias[4]} />
+
+      <Carousel category={dadosIniciais.categorias[5]} />
+
+      <Footer />
+    </AppWrapper>
   );
 }
 
